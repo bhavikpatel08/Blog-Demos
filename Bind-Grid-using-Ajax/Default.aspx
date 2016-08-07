6cf0257949e3b7c844ee37e7ffde5ba6f57790e0
Bhavik Patel <bhavikpatel08@gmail.com>
2016-08-07 08:42:08 +0530

    add Bind grid using ajax
    get more info about bind grid using ajax at
    http://www.dotnetspan.com/2015/02/bind-gridview-using-jquery-json-ajaxcall-in-aspnet.html

---
+++ Bind-Grid-using-Ajax/Default.aspx
@@ -0,0 +1,39 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Export Grid to Excel</title>
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script type="text/javascript">
+        $(document).ready(function () {
+            $("#btnShowData").click(function () {
+                $.ajax({
+                    type: "POST",
+                    contentType: "application/json; charset=utf-8",
+                    url: "Default.aspx/BindEmployees",
+                    data: "{}",
+                    dataType: "json",
+                    success: function (result) {
+                        for (var i = 0; i < result.d.length; i++) {
+                            $("#gvData").append("<tr><td>" + result.d[i].FirstName + "</td><td>" + result.d[i].LastName + "</td><td>" + result.d[i].City + "</td><td>" + result.d[i].Country + "</td></tr>");
+                        }
+                    },
+                    error: function (result) {
+                        alert("Error");
+                    }
+                });
+            });
+        });
+    </script>
+</head>
+<body>
+    <button id="btnShowData" runat="server">Show Data</button>
+    <br /><br />
+    <form id="form1" runat="server">
+        <asp:GridView ID="gvData" runat="server" CellPadding="4" ShowHeaderWhenEmpty="true" ForeColor="#333333">
+            <HeaderStyle BackColor="#507CD1" Font-Bold="True" ForeColor="White" />
+            <RowStyle BackColor="#EFF3FB" />
+        </asp:GridView>
+    </form>
+</body>
+</html>
---
+++ Bind-Grid-using-Ajax/Default.aspx.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Web.Services;
+using System.Web.UI;
+
+public partial class _Default : Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            BindDummyGridrow();
+        }
+    }
+
+    public void BindDummyGridrow()
+    {
+        DataTable dt = new DataTable();
+        dt.Columns.Add("FirstName");
+        dt.Columns.Add("LastName");
+        dt.Columns.Add("City");
+        dt.Columns.Add("Country");
+        gvData.DataSource = dt;
+        gvData.DataBind();
+    }
+
+    [WebMethod]
+    public static Employee[] BindEmployees()
+    {
+        string connectionString = @"Data Source=CIPL-PC72;Initial Catalog=NORTHWND;Integrated Security=True";
+        DataTable dt = new DataTable();
+        List<Employee> employeeList = new List<Employee>();
+        using (SqlConnection con = new SqlConnection(connectionString))
+        {
+            using (SqlCommand command = new SqlCommand("select FirstName, LastName, City, Country from Employees", con))
+            {
+                con.Open();
+                SqlDataAdapter da = new SqlDataAdapter(command);
+                da.Fill(dt);
+                foreach (DataRow dtrow in dt.Rows)
+                {
+                    Employee employee = new Employee();
+                    employee.FirstName = dtrow["FirstName"].ToString();
+                    employee.LastName = dtrow["LastName"].ToString();
+                    employee.City = dtrow["City"].ToString();
+                    employee.Country = dtrow["Country"].ToString();
+                    employeeList.Add(employee);
+                }
+            }
+        }
+
+        return employeeList.ToArray();
+    }
+
+    public class Employee
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+    }
+}
+
